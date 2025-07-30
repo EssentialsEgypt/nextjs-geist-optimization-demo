04e488949227abf34dc1d8723e3722c773a92b96
EssentialsEgypt <essentialsegy@gmail.com>
2025-07-30 04:47:24 +0300

    Add .next/static/chunks/src_app_page_tsx_9148afbf._.js 1753840041749

---
+++ .next/static/chunks/src_app_page_tsx_9148afbf._.js
@@ -0,0 +1,20 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_ui_c1bf3d91._.js",
+  "static/chunks/src_components_dashboard_cash-log_tsx_e9e0122d._.js",
+  "static/chunks/src_components_dashboard_audience-tracker_tsx_839b2c75._.js",
+  "static/chunks/src_components_dashboard_website-analytics_tsx_0178ec17._.js",
+  "static/chunks/src_components_dashboard_ad-idea-generator_tsx_8e493d8a._.js",
+  "static/chunks/src_components_dashboard_auto-messages_tsx_2df5f1b4._.js",
+  "static/chunks/src_components_dashboard_fb1943d5._.js",
+  "static/chunks/src_24081750._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_90f72504._.js",
+  "static/chunks/node_modules_recharts_es6_1a411cf4._.js",
+  "static/chunks/node_modules_@radix-ui_805caaee._.js",
+  "static/chunks/node_modules_@floating-ui_9ec1fa39._.js",
+  "static/chunks/node_modules_0b9b5b25._.js"
+],
+    source: "dynamic"
+});
